aecda5bd5e77ba36ddde9029cb53a9bd2a13d9ff
hira_ <hira_@Md-Chowdhury>
2022-05-05 20:10:24 -0500

    try 2...

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -4,7 +4,7 @@ public class DemoTwo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Git demo 2 ....");
-		
+		System.out.println("Git demo 2....");
+		System.out.println("Trying again....");
 	}
 }
